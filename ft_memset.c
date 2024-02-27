98eb407835d4aa34c6c9439d0e66ccd4a9a236af
Joseph <lievanosan@gmail.com>
2024-02-27 16:14:22 +0100

    Functions created: ft_bzero.c ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void    *ft_memset(void *str, int c, size_t len)
+{
+    size_t  i;
+
+    if (str == NULL)
+        return (NULL);
+    i = 0;
+    while (i < len)
+    {
+        ((unsigned char *)str)[i] = (unsigned char)c;
+        i++;
+    }
+    return (str);
+}
