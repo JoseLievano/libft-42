71aff2b66097d3a064a848c7908bd13232e27b38
Joseph <lievanosan@gmail.com>
2024-03-05 18:07:01 +0100

    Modify implementation of ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -12,7 +12,16 @@
 
 #include "libft.h"
 
-void ft_bzero(void *s, size_t n) 
+void ft_bzero(void *s, size_t n)
 {
-    ft_memset(s, '\0', n);
+    size_t	i;
+
+    if (!s)
+        return ;
+    i = 0;
+    while (i < n)
+    {
+        *(char*)(s + i) = 0;
+        i++;
+    }
 }
