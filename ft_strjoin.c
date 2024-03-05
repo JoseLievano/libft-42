0bfce3f63812346ae8c07427a032cf28c56cff30
Joseph <lievanosan@gmail.com>
2024-03-05 12:54:22 +0100

    Modify ft_strjoin.c to return if s1 or s2 are null

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -19,7 +19,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t			i;
 	size_t			j;
 
-	mem_space = sizeof (*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1);
+    if (!s1 || !s2)
+        return (NULL);
+    mem_space = sizeof (*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1);
 	new_str = (char *)malloc(mem_space);
 	if (!new_str)
 		return (NULL);
