9c7e45adefc600ec59f97d6d31caf1c60b8c096f
root <root@42-Home.myguest.virtualbox.org>
2024-03-06 01:35:23 +0100

    Norm ready

--- ft_split.c
+++ ft_split.c
@@ -12,90 +12,83 @@
 
 #include "libft.h"
 
-// Counts the number of words separated by the delimiter 'c'
-static size_t count_words(char const *s, char c)
+static size_t	count_words(char const *s, char c)
 {
-    size_t i;
-    size_t count;
+	size_t	i;
+	size_t	count;
 
-    i = 0;
-    count = 0;
-    while (s[i])
-    {
-        if (s[i] != c)
-        {
-            while (s[i] != c && s[i])
-                i++;
-            count++;
-        }
-        else
-            i++;
-    }
-    return (count);
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			while (s[i] != c && s[i])
+				i++;
+			count++;
+		}
+		else
+			i++;
+	}
+	return (count);
 }
 
-// Allocates memory for the split array
-static char **malloc_split(char const *s, char c)
+static char	**malloc_split(char const *s, char c)
 {
-    char **split;
+	char	**split;
 
-    split = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
-    return (split);
+	split = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
+	return (split);
 }
 
-// Frees the allocated memory for the split array in case of an error
-static void free_split(char **split, size_t k)
+static void	free_split(char **split, size_t k)
 {
-    while (k > 0)
-        free(split[--k]);
-    free(split);
+	while (k > 0)
+		free(split[--k]);
+	free(split);
 }
 
-// Allocates memory for a word and copies the word into it
-static char *malloc_word(char const *s, size_t start, size_t end)
+static char	*malloc_word(char const *s, size_t start, size_t end)
 {
-    char *word;
+	char	*word;
 
-    word = (char *)malloc(sizeof(char) * (end - start + 1));
-    if (!word)
-        return (NULL);
-    ft_strlcpy(word, s + start, end - start + 1);
-    return (word);
+	word = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!word)
+		return (NULL);
+	ft_strlcpy(word, s + start, end - start + 1);
+	return (word);
 }
 
-// Main function that splits the string into words
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    size_t i;
-    size_t j;
-    size_t k;
-    char **split;
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	**split;
 
-    if (!s)
-        return (NULL);
-    split = malloc_split(s, c);
-    if (!split)
-        return (NULL);
-    i = 0;
-    k = 0;
-    while (s[i])
-    {
-        if (s[i] != c)
-        {
-            j = i;
-            while (s[i] != c && s[i])
-                i++;
-            split[k] = malloc_word(s, j, i);
-            if (!split[k])
-            {
-                free_split(split, k);
-                return (NULL);
-            }
-            k++;
-        }
-        else
-            i++;
-    }
-    split[k] = NULL;
-    return (split);
+	split = malloc_split(s, c);
+	if (!split)
+		return (NULL);
+	i = 0;
+	k = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			j = i;
+			while (s[i] != c && s[i])
+				i++;
+			split[k] = malloc_word(s, j, i);
+			if (!split[k])
+			{
+				free_split(split, k);
+				return (NULL);
+			}
+			k++;
+		}
+		else
+			i++;
+	}
+	split[k] = NULL;
+	return (split);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,72 +12,70 @@
 
 #include "libft.h"
 
-// Check if a character 'c' is within the set 'set'.
-static int is_in_set(char c, char const *set)
+static int	is_in_set(char c, char const *set)
 {
-    while (*set)
-    {
-        if (c == *set)
-            return (1);
-        set++;
-    }
-    return (0);
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
 }
 
-// Handle special cases for NULL input and empty 'set'.
-static char *early_return(char const *s1, char const *set)
+static char	*early_return(char const *s1, char const *set)
 {
-    if (!s1)
-        return (NULL);
-    if (!set)
-        return (ft_strdup(s1));
-    return (NULL);
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	return (NULL);
 }
 
-// Find the start index of the string after trimming characters in 'set'.
-static size_t find_start(char const *s1, char const *set)
+static size_t	find_start(char const *s1, char const *set)
 {
-    size_t start = 0;
-    while (s1[start] && is_in_set(s1[start], set))
-        start++;
-    return start;
+	size_t	start;
+
+	start = 0;
+	while (s1[start] && is_in_set(s1[start], set))
+		start++;
+	return (start);
 }
 
-// Find the end index of the string before trimming characters in 'set'.
-static size_t find_end(char const *s1, char const *set, size_t start)
+static size_t	find_end(char const *s1, char const *set, size_t start)
 {
-    size_t end = ft_strlen(s1);
-    while (end > start && is_in_set(s1[end - 1], set))
-        end--;
-    return end;
+	size_t	end;
+
+	end = ft_strlen(s1);
+	while (end > start && is_in_set(s1[end - 1], set))
+		end--;
+	return (end);
 }
 
-// Allocate and create the trimmed string based on the start and end indices.
-static char *create_trimmed_str(char const *s1, size_t start, size_t end)
+static char	*create_trimmed_str(char const *s1, size_t start, size_t end)
 {
-    char *trimmed_str;
+	char	*trimmed_str;
 
-    trimmed_str = (char *)malloc(sizeof(char) * (end - start + 1));
-    if (!trimmed_str)
-        return (NULL);
-    if (start >= end)
-        trimmed_str[0] = '\0';
-    else
-        ft_strlcpy(trimmed_str, s1 + start, end - start + 1);
-    return trimmed_str;
+	trimmed_str = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!trimmed_str)
+		return (NULL);
+	if (start >= end)
+		trimmed_str[0] = '\0';
+	else
+		ft_strlcpy(trimmed_str, s1 + start, end - start + 1);
+	return (trimmed_str);
 }
 
-// The main function that uses helper functions to trim the string.
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    size_t start;
-    size_t end;
+	size_t	start;
+	size_t	end;
 
-    if (!s1 || !set)
-    {
-        return (early_return(s1, set));
-    }
-    start = find_start(s1, set);
-    end = find_end(s1, set, start);
-    return create_trimmed_str(s1, start, end);
+	if (!s1 || !set)
+	{
+		return (early_return(s1, set));
+	}
+	start = find_start(s1, set);
+	end = find_end(s1, set, start);
+	return (create_trimmed_str(s1, start, end));
 }
