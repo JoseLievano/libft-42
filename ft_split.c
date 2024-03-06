c020c555a697178349aad54107f07787d73ea3d9
Joseph <lievanosan@gmail.com>
2024-03-06 02:27:44 +0100

    Solving norminette

--- ft_split.c
+++ ft_split.c
@@ -33,14 +33,6 @@ static size_t	count_words(char const *s, char c)
 	return (count);
 }
 
-static char	**malloc_split(char const *s, char c)
-{
-	char	**split;
-
-	split = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
-	return (split);
-}
-
 static void	free_split(char **split, size_t k)
 {
 	while (k > 0)
@@ -59,16 +51,12 @@ static char	*malloc_word(char const *s, size_t start, size_t end)
 	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**spliter(char const *s, char c, char **split)
 {
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	char	**split;
 
-	split = malloc_split(s, c);
-	if (!split)
-		return (NULL);
 	i = 0;
 	k = 0;
 	while (s[i])
@@ -90,5 +78,16 @@ char	**ft_split(char const *s, char c)
 			i++;
 	}
 	split[k] = NULL;
+	return (split);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+
+	split = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!split)
+		return (NULL);
+	split = spliter(s, c, split);
 	return (split);
 }
