fa755db54308806776f84ad1bcf15da7b51c7bb8
Jose Lievano <jlievano@c2r10s3.42luxembourg.lu>
2024-03-07 12:04:06 +0100

    Almost finished version

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,17 @@
 /*   By: jlievano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 00:59:12 by jlievano          #+#    #+#             */
-/*   Updated: 2024/03/04 01:00:18 by jlievano         ###   ########.fr       */
+/*   Updated: 2024/03/07 11:09:23 by jlievano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	len_modifier(size_t *len, char const *str)
+{
+	if (*len > ft_strlen(str))
+		*len = ft_strlen(str);
+}
 
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
@@ -19,6 +25,9 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 
 	if (!str)
 		return (NULL);
+	if (start > ft_strlen(str))
+		return ('\0');
+	len_modifier(&len, str);
 	i = 0;
 	substr = (char *) malloc(sizeof(char) * (len + 1));
 	if (!substr)
