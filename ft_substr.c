2589bd12f62a06c76bb254ae1edbedc2dfa55628
root <root@42-Home.myguest.virtualbox.org>
2024-03-10 13:40:38 +0100

    Substr solved

--- ft_substr.c
+++ ft_substr.c
@@ -9,13 +9,13 @@
 /*   Updated: 2024/03/07 11:09:23 by jlievano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
 #include "libft.h"
 
-static void	len_modifier(size_t *len, char const *str)
+static void	len_modifier(size_t *len, unsigned int start, char const *str)
 {
-	if (*len > ft_strlen(str))
-		*len = ft_strlen(str);
+	if (*len > ft_strlen(str) - start)
+		*len = ft_strlen(str) - start;
 }
 
 char	*ft_substr(char const *str, unsigned int start, size_t len)
@@ -27,7 +27,7 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 		return (NULL);
 	if (start > ft_strlen(str))
 		return ('\0');
-	len_modifier(&len, str);
+	len_modifier(&len, start, str);
 	i = 0;
 	substr = (char *) malloc(sizeof(char) * (len + 1));
 	if (!substr)
@@ -39,4 +39,49 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 	}
 	substr[i] = '\0';
 	return (substr);
+}
+*/
+
+#include "libft.h"
+
+static size_t	str_len(char const *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(str + i))
+		i++;
+	return (i);
+}
+
+static char	*str_new(size_t n)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (n + 1));
+	if (!str)
+		return (NULL);
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	char	*str_ptr;
+
+	if (!s)
+		return (NULL);
+	if (start > str_len(s))
+		len = 0;
+	else if (len > (str_len(s) - start))
+		len = str_len(s) - start;
+	str = str_new(len);
+	if (!str)
+		return (NULL);
+	s += start;
+	str_ptr = str;
+	*(str + len) = '\0';
+	while (len-- && *s)
+		*str++ = *s++;
+	return (str_ptr);
 }
