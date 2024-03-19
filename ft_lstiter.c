3be75a21116c5f0d2a29ff0fbb5567ee05645c0c
Joseph <lievanosan@gmail.com>
2024-03-19 09:07:17 +0100

    Eliminate the use of a temp variable

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: jlievano <jlievano@student.42luxembourg.>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 08:51:47 by jlievano          #+#    #+#             */
-/*   Updated: 2024/03/19 08:59:52 by jlievano         ###   ########.fr       */
+/*   Updated: 2024/03/19 09:07:05 by jlievano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 void	ft_lstiter(t_list *lst, void (* f)(void *))
 {
-	t_list *temp;
-
 	if (!lst)
 		return ;
-	temp = lst;
-	while (temp)
+	while (lst)
 	{
-		(*f)(temp->content);
-		temp = lst->next;
+		(*f)(lst->content);
+		lst = lst->next;
 	}
 }
